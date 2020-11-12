ef33e8fe0eac0cd29619bfcd0ccef5444878587a
motatoes <moe.habib9@gmail.com>
2020-11-12 15:51:09 +0000

    fix python paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,32 +19,13 @@ WORKDIR=${SRCDIR:-/src}
 
 SPEC_FILE=${4:-*.spec}
 
-/root/.pyenv/shims/python -m pip install --upgrade pip wheel setuptools
-
-#
-# In case the user specified a custom URL for PYPI, then use
-# that one, instead of the default one.
-#
-if [[ "$PYPI_URL" != "https://pypi.python.org/" ]] || \
-   [[ "$PYPI_INDEX_URL" != "https://pypi.python.org/simple" ]]; then
-    # the funky looking regexp just extracts the hostname, excluding port
-    # to be used as a trusted-host.
-    mkdir -p /root/.pip
-    echo "[global]" > /root/.pip/pip.conf
-    echo "index = $PYPI_URL" >> /root/.pip/pip.conf
-    echo "index-url = $PYPI_INDEX_URL" >> /root/.pip/pip.conf
-    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> /root/.pip/pip.conf
-
-    echo "Using custom pip.conf: "
-    cat /root/.pip/pip.conf
-fi
 
 cd $WORKDIR
 
 if [ -f requirements.txt ]; then
-    /root/.pyenv/shims/pip install -r requirements.txt
+    pip install -r requirements.txt
 fi # [ -f requirements.txt ]
 
-/root/.pyenv/shims/pyinstaller --clean -y --dist ./dist/linux --workpath /tmp $SPEC_FILE
+pyinstaller --clean -y --dist ./dist/linux --workpath /tmp $SPEC_FILE
 
 chown -R --reference=. ./dist/linux
